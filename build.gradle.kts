976b0f1321e03e40b68491da8e17aeeda717dae4
GonSov <lpack0512@gmail.com>
2025-07-27 14:05:12 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,22 @@
+plugins {
+    kotlin("js") version "1.9.21"
+}
+
+group = "com.sov.zurvivalizm"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    js(IR) {
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+            binaries.executable()
+        }
+    }
+}
 
